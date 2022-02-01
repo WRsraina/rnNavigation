be53602e42b44fcff34a960ad3c2d161605baad2
Sumeet Raina <sraina@worldremit.com>
2022-02-01 17:43:53 +0000

    Passing params from CreatePostScreen to HomeScreen

--- App.js
+++ App.js
@@ -1,27 +1,45 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import * as React from 'react';
+import {Text, TextInput, View, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({navigation, route}) => {
   return (
-    // eslint-disable-next-line react-native/no-inline-styles
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
       <Button
-        title="Go to DetailsScreen"
-        onPress={() => navigation.navigate('Details')}
+        title="Create post"
+        onPress={() => navigation.navigate('CreatePost')}
       />
+      <Text style={{margin: 10}}>Post: {route.params?.post}</Text>
     </View>
   );
 };
 
-const DetailsScreen = () => {
+const CreatePostScreen = ({navigation, route}) => {
+  const [postText, setPostText] = React.useState('');
+
   return (
-    // eslint-disable-next-line react-native/no-inline-styles
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Details Screen</Text>
-    </View>
+    <>
+      <TextInput
+        multiline
+        placeholder="What's on your mind?"
+        style={{height: 200, padding: 10, backgroundColor: 'white'}}
+        value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {
+          // Pass and merge params back to home screen
+          navigation.navigate({
+            name: 'Home',
+            params: {post: postText},
+            merge: true,
+          });
+        }}
+      />
+    </>
   );
 };
 
@@ -36,7 +54,7 @@ const App = () => {
           component={HomeScreen}
           options={{title: 'Overview'}}
         />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
